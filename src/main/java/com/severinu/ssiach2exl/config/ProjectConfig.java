f74ae9cffa765a2211fa17cf6e91724d75078cb5
SeverinuM <sewerynmichota@gmail.com>
2021-09-13 04:10:56 +0100

    v2 final

--- src/main/java/com/severinu/ssiach2exl/config/ProjectConfig.java
+++ src/main/java/com/severinu/ssiach2exl/config/ProjectConfig.java
@@ -1,33 +1,27 @@
 package com.severinu.ssiach2exl.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 public class ProjectConfig extends WebSecurityConfigurerAdapter {
 
-    @Bean
-    public UserDetailsService userDetailsService() {
-        InMemoryUserDetailsManager userDetailService = new InMemoryUserDetailsManager();
-        User user = (User) User.withUsername("john")
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        var userDetailService = new InMemoryUserDetailsManager();
+        var user = User.withUsername("john")
                 .password("123456")
                 .authorities("read")
                 .build();
-
         userDetailService.createUser(user);
-        return userDetailService;
-    }
 
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return NoOpPasswordEncoder.getInstance();
+        auth.userDetailsService(userDetailService)
+                .passwordEncoder(NoOpPasswordEncoder.getInstance());
     }
 
     @Override
